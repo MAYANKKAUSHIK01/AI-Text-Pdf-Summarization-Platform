95a022ad2e25f9095cfe1f7722382ba0ec3ab062
MAYANKKAUSHIK01 <mayankkaushik730@gmail.com>
2026-05-10 12:11:34 +0530

    initial

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,9 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://ai-text-pdf-summarization-platform.vercel.app/",
+}));
 
 app.use(express.json());
 
